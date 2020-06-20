3e49b01cf3951ba945fd418d1b45516818bf8d74
Mark Smith <msmith4@gmail.com>
2020-06-20 04:24:58 +0000

    Added express, swagger, jest

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const pathToSwaggerUi = require('swagger-ui-dist').absolutePath()
+
+const app = express()
+app.use(express.static(pathToSwaggerUi))
+
+app.listen(3000)
