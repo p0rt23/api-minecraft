11937218b3d9d865493b2f85940073347c6a59b4
Mark Smith <msmith4@gmail.com>
2020-06-20 13:12:47 -0600

    Setup swagger and cleaned up project

---
+++ api/routes/docs.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const swaggerPath = require('swagger-ui-dist').getAbsoluteFSPath()
+
+const router = express.Router()
+
+router.use('/', express.static(swaggerPath))
+
+module.exports = router
