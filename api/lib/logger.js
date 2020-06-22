78a4ac326cab8647148611ddf035138d2faf5b86
Mark Smith <msmith4@gmail.com>
2020-06-22 02:42:33 +0000

    Fixing merge issues

--- api/lib/logger.js
+++ api/lib/logger.js
@@ -4,10 +4,6 @@ const bunyan = require('bunyan')
 const config = require('../config')
 
 module.exports = bunyan.createLogger({
-<<<<<<< HEAD
-  name: config.app_name,
-=======
   name: config.appName,
->>>>>>> develop
   level: 'debug'
 })
